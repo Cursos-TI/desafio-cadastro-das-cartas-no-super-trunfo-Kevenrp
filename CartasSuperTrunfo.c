8f2d44f0f7e466676d0761e245f990f955d1cca4
Kevenrp <90074669+Kevenrp@users.noreply.github.com>
2025-01-23 02:49:08 -0300

    Atualizar o CartasSuperTrunfo.c
    sistema completo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,41 +1,40 @@
 #include <stdio.h>
+int main()
+{
+	char  Carta[3];
+	char Estado[18], Nome[25];
+	int Populacao, Pturismo;
+	float Area, PIB;
 
-int main() {
+	printf("Digite o Nome do Estado: ");
+	scanf(" %s", &Estado);
 
-#include <stdio.h>
- int main() {
+	printf("Digite o Codigo da Carta: ");
+	scanf(" %s", &Carta);
 
-    char estado[18];
-    char carta[2];
-    char Nome[25];
-    int Populacao, Pturismo;
-    float Area, PIB;
-   
-    printf("Digite o Nome do Estado:\n ");
-    scanf(" %s", &estado);
+	printf("Digite o Nome da Cidade: ");
+	scanf(" %s", &Nome);
 
-    printf("Digite o Codigo da Carta:\n ");
-    scanf(" %a", &carta);
+	printf("Digite o Número de habitantes: ");
+	scanf(" %d", &Populacao);
 
-    printf("Digite o Nome da Cidade:\n ");
-    scanf(" %n", &Nome);
+	printf("Digite a Área da cidade: ");
+	scanf(" %f", &Area);
 
-    printf("Digite o Número de habitantes:\n ");
-    scanf(" %n", &Populacao);
+	printf("Digite o PIB da Cidade: ");
+	scanf(" %f", &PIB);
 
-    printf("Digite a Área da cidade:\n ");
-    scanf(" %c", &Area);
+	printf("Digite o Número de Pontos Turísticos: ");
+	scanf(" %d", &Pturismo);
 
-    printf("Digite o PIB da Cidade:\n ");
-    scanf(" %f", &PIB);
+	
+	printf("Estado: %s\n", Estado);
+	printf("Codigo da carta: %s\n", Carta);
+	printf("Nome da Cidade: %s\n", Nome);
+	printf("População: %d\n", Populacao);
+	printf("Área: %.1f km²\n", Area);
+	printf("PIB: %.1f milhões de reais\n", PIB);
+	printf("Número de Pontos Turisticos : %d\n", Pturismo);
 
-    printf("Digite o Número de Pontos Turísticos:\n ");
-    scanf(" %q", &Pturismo);
-
-    printf("Estado: %s", estado);
-
-return 0;
- 
-
-    return 0;
+	return 0;
 }
